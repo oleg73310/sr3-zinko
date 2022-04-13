acd3fa694d5807ef8a4b6d82f495acf15346b3e2
oleg1112 <103425617+oleg73310@users.noreply.github.com>
2022-04-13 19:16:45 +0000

    1

--- .ccls-cache/@home@runner@sr3-zinko/main.cpp
+++ .ccls-cache/@home@runner@sr3-zinko/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
-namespace std;
-int main() {
+using namespace std;
+void ff(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] % 2 == 0)
+            a[i] = 0;
+    }
+
+}
+int main()
+
+{
+    setlocale(0, ".1251");
+    int arr[14];
+   
+    
+    for (int i = 0;i < 14;i++)
+        cin >> arr[i];
+    ff(arr, 14);
+    for (int i = 0;i < 14;i++)
+        cout << arr[i] << " ";
+    cout << endl;
 
 
-  
+    system("pause");
+    return 0;
+
+
 }
